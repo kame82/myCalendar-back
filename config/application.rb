0ebbb0698191d874ed26a90848766e530b04ab98
kame_mac <kametyaso@gmail.com>
2025-03-22 21:28:20 +0900

    初期設定：backの設定を更新

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,11 @@ module App
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.api_only = true
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone= :local
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
