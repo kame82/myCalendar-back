733a7917bd43171638a5fd1036607e4169c31a9a
kame_mac <kametyaso@gmail.com>
2025-03-24 21:12:42 +0900

    Rails初期設定：rails generate時の不要ファイル生成を抑制

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,11 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.test_framework false
+    end
   end
 end
