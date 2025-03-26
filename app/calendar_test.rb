098ea909d2c2d7cae646cfa35277c8d28f08fd5a
kame_mac <kametyaso@gmail.com>
2025-03-26 13:36:33 +0900

    googleAuth認証の追加

--- app/calendar_test.rb
+++ app/calendar_test.rb
@@ -1,14 +1,39 @@
 require "google/apis/calendar_v3"
 require "googleauth"
 require "googleauth/stores/file_token_store"
+require 'dotenv/load'
 
 class GoogleCalendar
   def initialize
-    Calendar = Google::Apis::CalendarV3
-    @calendar = Calendar::CalendarService.new
+    calendar = Google::Apis::CalendarV3
+    # カレンダー操作用のインスタンスを生成、@serviceとして投入
+    @calendar = calendar::CalendarService.new
+    # アプリケーションの名前を設定（GCPで設定したサービスアカウント名）
     @calendar.client_options.application_name = ENV['GOOGLE_CALENDAR_APPLICATION_NAME']
+    # authorizeメソッド（Google::Auth::UserAuthorizer）から受け取った認証情報
     @calendar.authorization = authorize
-    @calendar_id = ENV[GOOGLE_CALENDAR_ID]
+    # 利用するカレンダーのID(GCPで設定したメールアドレス)を設定する
+    @calendar_id = ENV['GOOGLE_CALENDAR_ID']
+    puts "Google Calendar API initialized"
+  end
+
+  def authorize
+    client_secret = ENV['GOOGLE_CALENDAR_SECRET']
+    # 認証情報を取得し、credentialに格納
+    credential = Google::Auth::ServiceAccountCredentials.make_creds(
+      # json_ke_io: StringIO.new(client_secret),
+      json_key_io: File.open(client_secret),
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR
+    )
+    # アクセストークンを取得
+    credential.fetch_access_token!
+    # 認証情報を返す
+    credential
+
+    puts "Google Calendar API authorized"
   end
 
 end
+
+# クラスのインスタンスを作成し、メソッドを呼び出す
+ GoogleCalendar.new
