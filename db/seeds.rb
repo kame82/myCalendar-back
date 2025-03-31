c2724966dfbc5ec97ab4bdd550fd3aedd067660d
kame_mac <kametyaso@gmail.com>
2025-03-31 20:27:16 +0900

    不要のため削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,3 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-Post.create!(
-  [
-    { title: '1234' },
-    { title: 'abcd' },
-    { title: 'fghj' },
-  ]
-)
