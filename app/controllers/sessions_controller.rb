0c6286a1eb39d783f1a5a11cb6ef26a3e66deaaf
kame_mac <kametyaso@gmail.com>
2025-04-02 15:50:29 +0900

    session controllerの更新

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,33 @@
 class SessionsController < ApplicationController
+
   def create
-    auth = request.env["omniauth.auth"]
-    puts "Provider: #{auth[:provider]}"
-    puts "UID: #{auth[:uid]}"
-    puts "Name: #{auth[:info][:name]}"
-    puts "Email: #{auth[:info][:email]}"
-    redirect_to root_url
+    frontend_url = ENV['REACT_APP_API_URL']
+    user_info = request.env['omniauth.auth']
+    google_user_id = user_info['uid']
+    provider = user_info['provider']
+    token = generate_token_with_google_user_id(google_user_id, provider)
+
+    user_authentication = UserAuthentication.find_by(uid: google_user_id, provider: provider)
+
+    if user_authentication
+      Rails.logger.info("User already exists")
+      redirect_to "#{frontend_url}/?token=#{token}",allow_other_host: true
+    else
+      Rails.logger.info.("User does not exist")
+      user = User.create(name: user_info['info']['name'], email:user_info['info']['email'])
+      UserAuthentication.create( user_id: user.id, uid: google_user_id, provider: provider)
+      redirect_to "#{frontend_url}/?token=#{token}", allow_other_host: true
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def generate_token_with_google_user_id(google_user_id, provider)
+    exp = Time.now.to_i + 24 *3600
+    payload = { google_user_id: google_user_id, provider: provider, exp: exp }
+    JWT.encode( payload, hmac_secret, 'HS256' )
   end
 end
