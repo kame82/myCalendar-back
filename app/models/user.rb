e7ad6eba09b265c62106b259bad18fcbe2fa8c60
kame_mac <kametyaso@gmail.com>
2025-04-02 17:02:05 +0900

    userモデルとuser_authenticationモデルのリレーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
+
+  has_one :user_authentication, dependent: :destroy
 end
--- app/models/user_authentication.rb
+++ app/models/user_authentication.rb
@@ -1,3 +1,3 @@
 class UserAuthentication < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
 end
