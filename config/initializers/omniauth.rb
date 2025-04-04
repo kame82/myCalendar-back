0cc7f87d6c51807e9fbe5b8c128d3f32a99c150e
kame_mac <kametyaso@gmail.com>
2025-04-04 13:02:29 +0900

    scopeの設定

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  scope: 'userinfo.email, userinfo.profile, openid, https://www.googleapis.com/auth/calendar.events.owned',
+  access_type: 'offline'
 end
 OmniAuth.config.allowed_request_methods = [:post, :get]
