aee075eb0fcf44f92bc9930f64bad9d0c8bdb0db
kame_mac <kametyaso@gmail.com>
2025-04-05 11:27:34 +0900

    refresh memory不要のため削除

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
   scope: 'userinfo.email, userinfo.profile, openid, https://www.googleapis.com/auth/calendar.events.owned',
   access_type: 'offline'
+  # prompt: 'consent' #一時的に追加
 end
 OmniAuth.config.allowed_request_methods = [:post, :get]
